cb41ffc4a5efa690fa1910c61ca2768598605981
thibault <thibault.blanchard@edu.itescia.fr>
2022-01-19 13:13:38 +0100

    gameplay.xml with progressBar and score MainMenu ProgressBar pour 10 essais

--- app/src/main/java/com/codingfactory/fruitroulette/ui/MainMenu.java
+++ app/src/main/java/com/codingfactory/fruitroulette/ui/MainMenu.java
@@ -32,4 +32,8 @@ public class MainMenu extends AppCompatActivity {
 //        newIntent.putExtra();
         startActivity(quitIntent);
     }
+
+    /*public int progressBarIncrement (){
+
+    }*/
 }
