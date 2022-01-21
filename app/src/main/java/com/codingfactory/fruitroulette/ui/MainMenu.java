44109a5726971b8406b148a47b6db35c32e8ffb9
thibault <thibault.blanchard@edu.itescia.fr>
2022-01-21 08:39:22 +0100

    Quit on main menu fonctionnal

--- app/src/main/java/com/codingfactory/fruitroulette/ui/MainMenu.java
+++ app/src/main/java/com/codingfactory/fruitroulette/ui/MainMenu.java
@@ -2,18 +2,30 @@ package com.codingfactory.fruitroulette.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.codingfactory.fruitroulette.R;
 
 public class MainMenu extends AppCompatActivity {
 
+    TextView quit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+
+            quit=findViewById(R.id.quitButton);
+            quit.setOnClickListener(view1 -> {
+                finish();
+                System.exit(0);
+            });
     }
 
     public void highScores(View view) {
@@ -27,13 +39,5 @@ public class MainMenu extends AppCompatActivity {
 //        newIntent.putExtra();
         startActivity(gameIntent);
     }
-    public void quit(View view) {
-        Intent quitIntent = new Intent(MainMenu.this, Quit.class);
-//        newIntent.putExtra();
-        startActivity(quitIntent);
-    }
 
-    /*public int progressBarIncrement (){
-
-    }*/
 }
