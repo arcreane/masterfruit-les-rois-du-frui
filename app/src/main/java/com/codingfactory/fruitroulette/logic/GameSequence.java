b4ffbb310805de3b569c67b413ddf8d259272c64
vieiraruben <86010862+vieiraruben@users.noreply.github.com>
2022-01-21 13:23:27 +0100

    Commented GameSequence class

--- app/src/main/java/com/codingfactory/fruitroulette/logic/GameSequence.java
+++ app/src/main/java/com/codingfactory/fruitroulette/logic/GameSequence.java
@@ -1,5 +1,7 @@
 package com.codingfactory.fruitroulette.logic;
 
+import android.util.Log;
+
 import com.codingfactory.fruitroulette.fruit.Fruity;
 import com.codingfactory.fruitroulette.ui.RecyclerAdapter;
 
@@ -16,6 +18,7 @@ public class GameSequence {
     private List<Integer> hiddenFruit;
     private RecyclerAdapter adapter;
 
+    //Constructor that initializes all parameters for the game sequence.
     public GameSequence() {
         this.attempts = 10;
         this.fruitDiscovered = 0;
@@ -30,14 +33,11 @@ public class GameSequence {
                 Fruity.PLUM,
                 Fruity.GRAPES,
                 Fruity.LEMON
-                ));
+        ));
         this.hiddenFruit = fruitGenerator();
     }
 
-    public List<Fruity> getPossibleFruit() {
-        return this.possibleFruit;
-    }
-
+    //Generates a new combination of four fruits.
     private List<Integer> fruitGenerator() {
         List<Integer> hiddenFruit = new ArrayList<>();
         Random random = new Random();
@@ -45,13 +45,12 @@ public class GameSequence {
             int fruit = random.nextInt(8);
             if (!hiddenFruit.contains(fruit)) hiddenFruit.add(fruit);
         }
-        System.out.println("hidden 0: " + hiddenFruit.get(0));
-        System.out.println("hidden 1: " + hiddenFruit.get(1));
-        System.out.println("hidden 2: " + hiddenFruit.get(2));
-        System.out.println("hidden 3: " + hiddenFruit.get(3));
+        System.out.println("hidden 0: " + hiddenFruit.get(0) + "\nhidden 1: " + hiddenFruit.get(1) +
+                "\nhidden 2: " + hiddenFruit.get(2) + "\nhidden 3: " + hiddenFruit.get(3));
         return hiddenFruit;
     }
 
+    //Returns a list of images that represent hints (either seeds or peel based on argument).
     public void getHint(int whichHint) {
         String hintImg;
         if (whichHint == 1) hintImg = "ic_seeds";
@@ -74,6 +73,7 @@ public class GameSequence {
         adapter.newLine(seedImg);
     }
 
+    //Checks if user has enough attempts to get a hint.
     public boolean canIGetAHint() {
         if (firstHintGiven) return this.attempts - 3 > 0;
         else return this.attempts - 2 > 0;
@@ -95,7 +95,9 @@ public class GameSequence {
             }
             recyclerLine[i] = getPossibleFruit().get(guessed[i]).getImg();
         }
+        //Sends RecyclerAdapter images corresponding to the guessed fruit.
         adapter.newLine(recyclerLine);
+        //Sends RecyclerAdapter number of correctly positioned fruit and number of misplaced fruit.
         adapter.addPositions(rightPosition, wrongPosition);
         if (fruitDiscovered == 4) {
             cumulatedScore += attempts + 1;
@@ -104,40 +106,53 @@ public class GameSequence {
         return roundOver();
     }
 
+    //Resets attempts, clears RecyclerAdapter and generates new fruit.
     public void newRound() {
         attempts = 10;
         adapter.clear();
         hiddenFruit = fruitGenerator();
     }
 
+    //Receives RecyclerAdapter from NewGame so that guesses can be shown.
     public void setAdapter(RecyclerAdapter adapter) {
         this.adapter = adapter;
     }
 
+    //Returns number of attemps left for current round.
     public int getAttempts() {
         return this.attempts;
     }
 
+    //Checks if user has won.
     public boolean isItAWin() {
         return fruitDiscovered == 4;
     }
 
+    //Checks if the round is over (either user has won or no more attempts are left).
     public boolean roundOver() {
         return (attempts == 0 || isItAWin());
     }
 
+    //Resets game and starts new round.
     public void reset() {
-        newRound();
         fruitDiscovered = 0;
         cumulatedScore = 0;
         round = 0;
+        newRound();
     }
 
+    //Getter for current round number.
     public int getRound() {
         return this.round;
     }
 
+    //Getter for total score.
     public int getCumulatedScore() {
         return this.cumulatedScore;
+    }
+
+    //Getter for all fruits.
+    public List<Fruity> getPossibleFruit() {
+        return this.possibleFruit;
     }
 }
